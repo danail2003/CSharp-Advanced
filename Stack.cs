0618ea587fd31d4a1be2e486264decbcf4eb3b57
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-10-19 19:53:38 +0300

    Add files via upload

---
+++ Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private readonly List<T> data;
+        private int index = -1;
+
+        public Stack()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Push(params T[] items)
+        {
+            foreach (var item in items)
+            {
+                data.Insert(++index, item);
+            }
+        }
+
+        public void Pop()
+        {
+            if (index < 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            index--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = index; i >= 0; i--)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
