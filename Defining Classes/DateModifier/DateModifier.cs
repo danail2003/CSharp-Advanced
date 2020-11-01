7df373a69137a7cb340f6ab8ccb2d2e535f98db5
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-01 20:03:36 +0200

    Add files via upload

---
+++ Defining Classes/DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public int GettingDiffBetweenTwoDates(string firstDate, string secondDate)
+        {
+            DateTime startingDay = DateTime.Parse(firstDate);
+            DateTime endDay = DateTime.Parse(secondDate);
+
+            int result = (startingDay - endDay).Days;
+            int absolute = Math.Abs(result);
+
+            return absolute;
+        }
+    }
+}
---
+++ Defining Classes/DateModifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace DateModifier
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+
+            Console.WriteLine(dateModifier.GettingDiffBetweenTwoDates(firstDate, secondDate));
+        }
+    }
+}
