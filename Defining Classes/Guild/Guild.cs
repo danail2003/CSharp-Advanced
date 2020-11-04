f5099a44d0789e673f6915c75617806893152ab1
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-04 20:44:29 +0200

    Add files via upload

---
+++ Defining Classes/Guild/Guild.cs
@@ -0,0 +1,87 @@
+﻿namespace Guild
+{
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+
+    public class Guild
+    {
+        private readonly List<Player> players;
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.players = new List<Player>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => this.players.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if (this.Capacity > this.players.Count)
+            {
+                this.players.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            foreach (var player in players)
+            {
+                if (player.Name == name)
+                {
+                    this.players.Remove(player);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player player = this.players.FirstOrDefault(x => x.Name == name);
+
+            if (player.Rank == "Trial")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player player = this.players.FirstOrDefault(x => x.Name == name);
+
+            if (player.Rank == "Member")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string classw)
+        {
+            Player[] kickedPlayers = this.players.Where(x => x.Class == classw).ToArray();
+            this.players.RemoveAll(x => x.Class == classw);
+
+            return kickedPlayers;
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Players in the guild: {this.Name}");
+
+            foreach (var player in players)
+            {
+                stringBuilder.AppendLine($"Player {player.Name}: {player.Class}");
+                stringBuilder.AppendLine($"Rank: {player.Rank}");
+                stringBuilder.AppendLine($"Description: {player.Description}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Defining Classes/Guild/Player.cs
@@ -0,0 +1,31 @@
+﻿namespace Guild
+{
+    using System.Text;
+
+    public class Player
+    {
+        public Player(string name, string classw)
+        {
+            this.Name = name;
+            this.Class = classw;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Player {Name}: {Class}");
+            stringBuilder.AppendLine($"Rank: {Rank}");
+            stringBuilder.AppendLine($"Description: {Description}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
