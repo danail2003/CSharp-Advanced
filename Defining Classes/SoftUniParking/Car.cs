eea5d9e13a5c29cb17c8cc56fb6c4d4d22b0eeeb
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-07 10:41:38 +0200

    Add files via upload

---
+++ Defining Classes/SoftUniParking/Car.cs
@@ -0,0 +1,34 @@
+﻿namespace SoftUniParking
+{
+    using System.Text;
+
+    public class Car
+    {
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Make: {this.Make}");
+            stringBuilder.AppendLine($"Model: {this.Model}");
+            stringBuilder.AppendLine($"HorsePower: {this.HorsePower}");
+            stringBuilder.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
