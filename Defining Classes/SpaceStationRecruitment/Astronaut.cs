a6f2f43c10188e7323b6d3eec3940375118c4ceb
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-07 10:42:11 +0200

    Add files via upload

---
+++ Defining Classes/SpaceStationRecruitment/Astronaut.cs
@@ -0,0 +1,44 @@
+﻿namespace SpaceStationRecruitment
+{
+    using System.Text;
+
+    public class Astronaut
+    {
+        private string name;
+        private int age;
+        private string country;
+
+        public Astronaut(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public string Country
+        {
+            get { return this.country; }
+            set { this.country = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Astronaut: {this.Name}, {this.Age} ({this.Country})");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Defining Classes/SpaceStationRecruitment/SpaceStation.cs
@@ -0,0 +1,87 @@
+﻿namespace SpaceStationRecruitment
+{
+    using System.Text;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class SpaceStation
+    {
+        private List<Astronaut> astronauts;
+        private string name;
+        private int capacity;
+
+        public SpaceStation(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Astronaut = new List<Astronaut>();
+        }
+
+        public List<Astronaut> Astronaut
+        {
+            get { return this.astronauts; }
+            set { this.astronauts = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Capacity
+        {
+            get { return this.capacity; }
+            set { this.capacity = value; }
+        }
+
+        public int Count => astronauts.Count;
+
+        public void Add(Astronaut astronaut)
+        {
+            if (this.astronauts.Count < Capacity)
+            {
+                this.astronauts.Add(astronaut);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            foreach (var astronaut in astronauts)
+            {
+                if (astronaut.Name == name)
+                {
+                    this.astronauts.Remove(astronaut);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            Astronaut astronaut = astronauts.OrderByDescending(x => x.Age).FirstOrDefault();
+            return astronaut;
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            Astronaut astronaut = this.astronauts.FirstOrDefault(x => x.Name == name);
+            return astronaut;
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Astronauts working at Space Station {this.Name}:");
+
+            foreach (var astronaut in astronauts)
+            {
+                stringBuilder.AppendLine($"Astronaut: {astronaut.Name}, {astronaut.Age} ({astronaut.Country})");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
