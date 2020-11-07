5c70c16e3e432d923db7092c66d3f4c6b8fcb138
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-07 10:41:15 +0200

    Add files via upload

---
+++ Defining Classes/Repository/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace Repository
+{
+    using System;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+        private DateTime birthdate;
+
+        public Person(string name, int age, DateTime birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public DateTime Birthdate
+        {
+            get { return this.birthdate; }
+            set { this.birthdate = value; }
+        }
+    }
+}
---
+++ Defining Classes/Repository/Repository.cs
@@ -0,0 +1,76 @@
+﻿namespace Repository
+{
+    using System.Collections.Generic;
+
+    public class Repository
+    {
+        private Dictionary<int, Person> people;
+        private int id = 0;
+
+        public Repository()
+        {
+            this.People = new Dictionary<int, Person>();
+            this.ID = id;
+        }
+
+        public int Count => this.people.Count;
+
+        public Dictionary<int, Person> People
+        {
+            get { return this.people; }
+            set { this.people = value; }
+        }
+
+        public int ID
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public void Add(Person person)
+        {
+            this.people.Add(id++, person);
+        }
+
+        public Person Get(int id)
+        {
+            foreach (var person in people)
+            {
+                if (person.Key == id)
+                {
+                    return person.Value;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Update(int id, Person newPerson)
+        {
+            foreach (var person in people)
+            {
+                if (person.Key == id)
+                {
+                    this.people[id] = newPerson;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            foreach (var person in people)
+            {
+                if (person.Key == id)
+                {
+                    this.people.Remove(id);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
