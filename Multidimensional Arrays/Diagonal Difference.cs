4ec3c7f8357742f2d6b9cf86531bda45a31bff04
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-04-10 21:08:07 +0300

    Update and rename Diagonal Difference to Multidimensional Arrays/Diagonal Difference.cs

--- Multidimensional Arrays/Diagonal Difference.cs
+++ Multidimensional Arrays/Diagonal Difference.cs
@@ -24,6 +24,7 @@ namespace Diagonal_Difference
             int secondary = 0;
             int number = 0;
             int counter = 0;
+            
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
