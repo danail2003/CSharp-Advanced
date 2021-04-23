923867740e9a3d5539a977a79d41b2f8beb445bc
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-04-23 19:58:54 +0300

    Update and rename Reverse and Exclude to Delegates/Reverse and Exclude.cs

--- Delegates/Reverse and Exclude.cs
+++ Delegates/Reverse and Exclude.cs
@@ -9,8 +9,10 @@ namespace Reverse_And_Exclude
         static void Main()
         {
             Func<string, int> parser = new Func<string, int>(int.Parse);
+            
             int[] numbers = Console.ReadLine().Split().Select(parser).ToArray();
             int number = parser(Console.ReadLine());
+            
             Predicate<int> divisible = new Predicate<int>(x => x % number == 0);
             List<int> reverse = new List<int>();
 
@@ -25,6 +27,7 @@ namespace Reverse_And_Exclude
             reverse.Reverse();
             Action<int> print = x => Console.Write(x + " ");
             reverse.ToList().ForEach(print);
+            
             Console.WriteLine();
         }
     }
