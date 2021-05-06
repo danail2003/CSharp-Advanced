faa137b23a2313f379430c4ae0eb2e3a062412d4
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-05-06 10:24:58 +0300

    Update and rename TriFunction to Delegates/TriFunction.cs

--- Delegates/TriFunction.cs
+++ Delegates/TriFunction.cs
@@ -9,8 +9,10 @@ namespace TriFunction
         static void Main()
         {
             Func<string, int> parser = new Func<string, int>(int.Parse);
+            
             int number = parser(Console.ReadLine());
             List<string> names = Console.ReadLine().Split().ToList();
+            
             Func<string, int, bool> isMatch = (a, b) => a.Sum(c => c) >= b;
             Func<Func<string, int, bool>, List<string>, string> returnSum = (a, b) => b.FirstOrDefault(s => a(s, number));
             string result = returnSum(isMatch, names);
